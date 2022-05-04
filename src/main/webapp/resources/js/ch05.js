83209e8f31e8a244182b2c3be607570d77624cdc
HeoYujin <uwoorii@gmail.com>
2022-05-05 02:34:00 +0900

    20220505

--- src/main/webapp/resources/js/ch05.js
+++ src/main/webapp/resources/js/ch05.js
@@ -101,18 +101,37 @@ function init() {
 		setTimeout(function() { $("form").submit(); }, 8000); 
 		}
 		
-	var h = $("#background").height() - $("#dialogue").height();
-	if(h < 0) h = 0;
-	$("#background").height(h);
-		
-	var w = $("#scene").width() - $("#dialogue > img:first-of-type").width();
-	w = w - $("#choice").width();
-	if(w < 0) w = 0;
-	$("#scene").width(w);
-	
-	$("#dialogue").on("click", function() { $("form").submit(); });	
-	
-	$("#choice > img").height( $("#dialogue > img").height() );
+ w = $("#background").width() / 1920;
+   h = $("#background").height() / 1080;
+   x = $("#background").width() / 2.0;
+
+   $("#dialogue").height($("#dialogue").height() * h);
+   $("#dialogue_bg").height($("#dialogue").height());
+
+   var sw = $("#dialogue").width() - $("#dialogue > img:first-of-type").width();
+   sw = sw - $("#choice").width();
+   if (sw < 0) sw = 0;
+   $("#scene").width(sw);
+   
+   resize("#dialogue_img");
+   resize("#choice > img");
+
+   var temp = "auto " + (90*w) + "px auto " + (50*w) + "px";
+   $("#dialogue_img").css("margin", temp);
+   
+   temp = 55*h + "px " + 20*w + "px " + 10*h + "px 0";
+   $("#title > div:nth-child(1)").css("margin", temp);
+   
+   temp = 54*h + "px";
+   $("#title > div:nth-child(2)").css("margin-top", temp);
+   
+   temp = 100*h + "px " + 110*w + "px " + 80*h + "px " + 50*w + "px";
+   $("#choice > img").css("margin", temp);
+
+   $("#blur").height(260 * h);
+   $("#blur").css("left", "0");
+   $("#blur").css("bottom", "0");
+
 	
 	$("#dialogue").click(function() {
 		$("form").submit();
