b2c6d8adcb0d382c7bb31c7bbd27cff4f7519489
snowm <snowman4453@gmail.com>
2022-05-07 00:57:44 +0900

    commit upload files

---
+++ src/main/webapp/resources/js/shabti/shabti.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+/*캐러셀*/
+$(document).ready(function(){
+  $('#owl-UI').owlCarousel();
+});
+$('#owl-UI').owlCarousel({
+	nav:true, 
+	loop: true, 
+	dots: true, 
+	autoplay:true, 
+	rewind: true, 
+	autoplayTimeout: 2000, 
+	margin:10, 
+	responsiveClass:true, 
+	responsive:{ 
+		0:{ items:2, nav:true }, 
+		600:{ items:3, nav:false }, 
+		1000:{ items:6, nav:true, loop:false }
+	}
+
+	});
+/*창닫기 */
+function hide_dic(){
+	window.close();
+}
