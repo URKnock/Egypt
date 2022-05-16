db1f1867f685aa3e2a9199b51d46fc020afbed05
Knock <chein1010@naver.com>
2022-05-16 22:26:29 +0900

    20220516 restore

---
+++ src/main/webapp/WEB-INF/classes/js/interaction/ch05/DuatGameFirst.js
@@ -0,0 +1,169 @@
+var count = 0;
+
+function interaction() {	
+/*	$(".guide2")
+		.animate({opacity: "100%"}, 500)
+		.animate({opacity: "0%"}, 3000);*/
+		$("#background").children().hide();
+		$("form").hide();
+		$(".saja").css({left:"30%"});
+	
+
+	
+		setTimeout(function() {
+			$(".spear").css({opacity: 1});
+		}, 1800);
+
+		$(".spear").delay(2900).animate({left: "45%"}, 1000).fadeOut(1000);
+		
+		//$(".saja").delay(3200).animate({opacity: "0%"});
+			setTimeout(function() {
+			$(".saja").attr("src", 
+			"/D/resources/character/ch05/하얀창든사자.png");
+		}, 3300);
+			setTimeout(function() {
+			$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든멈춘사자.png");
+		}, 3800);
+		//$(".saja").delay(3300).animate({opacity: "100%"});
+		
+		$(".guide2").delay(4000).animate({opacity: "100%"}, 500).delay(2000).animate({opacity: "0%"}, 500);
+		
+/*		setTimeout(function() {
+			$(".saja").delay(5000).css({left:"-20%"});
+		}, 4200); */
+		
+		
+		//사자 대기중으로 바뀜
+		setTimeout(function() {
+		$(".saja").delay(5000).css({left:"-20%"});	
+		$(".saja").attr("src", 
+		"/D/resources/character/ch05/창든사자(위)_1처음.webp");
+		}, 5000); 
+		setTimeout(function() {
+		$(".saja").attr("src", 
+		"/D/resources/character/ch05/창든사자(위)_2대기.webp");
+		}, 5000); 		
+		//악어 튀어나오고
+		 setTimeout(function() {
+			console.log("셋탐 호출");
+			$(".crocsi4").attr("src", 
+			"/D/resources/object/ch05/악어_시작오.webp");
+		}, 6900); 
+		
+		setTimeout(function() {
+			console.log("셋탐 호출2");
+			$(".crocsi4").attr("src", 
+			"/D/resources/object/ch05/악어_진행오.webp");
+		}, 8400); 
+		
+		setTimeout(function() {
+			$(".crocsi3").attr("src", 
+			"/D/resources/object/ch05/악어_시작왼.webp");		
+		}, 6400);
+		
+		setTimeout(function() {
+			$(".crocsi3").attr("src", 
+			"/D/resources/object/ch05/악어_진행왼.webp");		
+		}, 7900);
+
+		setTimeout(function() {
+			$(".crocsi2").attr("src", 
+			"/D/resources/object/ch05/악어_시작오.webp");					
+		}, 5000); 	
+		
+		setTimeout(function() {
+			$(".crocsi2").attr("src", 
+			"/D/resources/object/ch05/악어_진행오.webp");
+		}, 6500); 		
+			
+		setTimeout(function() {
+			$(".crocsi1").attr("src", 
+			"/D/resources/object/ch05/악어_시작왼.webp");
+		}, 4900);
+		
+ 		setTimeout(function() {
+			$(".crocsi1").attr("src", 
+			"/D/resources/object/ch05/악어_진행왼.webp");
+		}, 6400); 
+		
+
+		$(".crocsi1").on("load", function() {
+			$(".crocsi1").one("click", function() {
+			console.log(count); 
+			$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(위)_3쏴.webp").stop();
+			//다시 원상태
+			setTimeout(function() {$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(위)_2대기.webp").stop(); }, 2000);
+			$(".crocsi1").attr("src", 
+			"/D/resources/object/ch05/악어_죽음왼.webp");
+			isclicked();
+			$(".crocsi1").fadeOut(2000);
+			if(count == 8) {
+				setTimeout("$('form').submit()", 3000);
+				}
+			
+			 });
+		});		
+		
+		$(".crocsi2").on("load", function() {
+			$(".crocsi2").one("click", function() {
+			$(".saja").css({'transform': "scaleX(-1)"});
+			$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(위)_3쏴.webp").stop();	
+			//다시 원상태
+			setTimeout(function() {$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(위)_2대기.webp").stop(); }, 2000);			
+			console.log(count); 
+			$(".crocsi2").attr("src", 
+			"/D/resources/object/ch05/악어_죽음오.webp");
+			isclicked();
+			$(".crocsi2").fadeOut(2000);
+		
+			if(count == 8) {
+				setTimeout("$('form').submit()", 3000);
+				}
+			
+			 });
+		});
+		
+		
+		
+		$(".crocsi3").on("load", function() {
+			$(".crocsi3").one("click", function() { 
+				console.log(count);
+			$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(하1)_3쏴.webp").stop();	
+			//다시 원상태
+			setTimeout(function() {$(".saja").attr("src", 
+			"/D/resources/character/ch05/창든사자(하1)_2대기.webp").stop(); }, 2000);						
+			$(".crocsi3").attr("src", 
+			"/D/resources/object/ch05/악어_죽음왼.webp");
+			isclicked();
+			$(".crocsi3").fadeOut(2000);
+			if(count == 8) {
+				setTimeout("$('form').submit()", 3000);
+				}
+			 });
+		});
+		
+		$(".crocsi4").on("load", function() {
+			$(".crocsi4").one("click", function() { 
+				$(".crocsi2").off("click");
+				console.log(count);
+			$(".crocsi4").attr("src", 
+			"/D/resources/object/ch05/악어_죽음오.webp");
+			isclicked();
+			$(".crocsi4").fadeOut(2000);
+			if(count == 8) {
+				setTimeout("$('form').submit()", 3000);
+				}
+			 });
+		});
+
+
+		function isclicked() {
+			count++;
+		}	
+}
---
+++ src/main/webapp/WEB-INF/classes/js/interaction/ch05/crocAttack.js
@@ -0,0 +1,15 @@
+function interaction() { 
+	$("#background").children().hide();
+	$("form").hide();
+	scene = $("input[name='scene']").val();
+	
+		$(".crocs4").attr("src", 
+		"/D/resources/object/ch05/악어인트로_시작오.webp");
+		$(".crocs3").attr("src", 
+		"/D/resources/object/ch05/악어인트로_시작왼.webp");	
+		$(".crocs2").attr("src", 
+		"/D/resources/object/ch05/악어인트로_시작오.webp");	
+		$(".crocs1").attr("src", 
+		"/D/resources/object/ch05/악어인트로_시작왼.webp");		
+		setTimeout("$('form').submit()", 2600);	
+}
